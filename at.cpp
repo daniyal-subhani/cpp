8281fed85bb2589f6f15487752f3910d1c99e071
daniyal-subhani <daniyalsubhani527@gmail.com>
2025-11-02 19:37:34 +0500

    committing

--- at.cpp
+++ at.cpp
@@ -5,6 +5,9 @@
 
 int main() {
     std::string text = "This is a simple string index example.";
+    std::string change = "hello";
+    change.at(0) = 'H';
+    std::cout << change << "\n";
     std::cout << text.at(0);
     return 0;
 }
